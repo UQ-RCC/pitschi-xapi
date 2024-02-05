60e86ac1288e58c6f1dc841ebe090c5ac3ac039f
mark <18689435+markendr@users.noreply.github.com>
2024-02-05 22:17:22 +1000

    allow environ vars in config file

--- pitschi/config.py
+++ pitschi/config.py
@@ -5,12 +5,13 @@ import errno
 config = configparser.ConfigParser()
 config.read(["conf/pitschixapi.conf", os.environ.get("PITSCHI_XAPI_CONFIG", ""), "/etc/pitschi/conf/pitschixapi.conf"])
 
-def get(section, option, default = None, required=False):
+def get(section, option, default = None, os_env=True, required=False):
     """
     Reads config optoin from the given section, returning default if not found
     """
+    cp_vars = os.environ if os_env else None
     try:
-        return config.get(section, option).strip()
+        return config.get(section, option, vars=cp_vars).strip()
     except:
         if required: 
             raise Exception(f"option {option} is required in section {section}")
